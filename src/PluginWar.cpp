8ca22da9d4a587c52fa13a0e9e80e99d0bd78642
Killian GOMES <E126956W@S040V7pc09.ensinfo.sciences.univ-nantes.prive>
2016-12-19 17:38:06 +0100

    identation

--- src/PluginWar.cpp
+++ src/PluginWar.cpp
@@ -21,125 +21,121 @@ PluginWar::~PluginWar(){
 void PluginWar::nextGeneration(Grid& gr)
 {
 
-  cout<<"inside plugin method nextgeneration() OF PLUGINWAR that outputs the next gen of the grid"<<endl;
-  vector<int> temp;
-  bool fought = false;
-  bool redMove = false;
-  bool blueMove = false;
-  int cpt = 0;
-  
-  //init the temp vector with 0 state
-  for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++)
-    {
-      temp.push_back(0);
-    }
-  
-  for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++)
-    {
-      
-      map<pair<int,int>,int> neighborsState = gr.getCellNeighborsState((gr.getTab())[ind]);
-      int cellsRed = 0;
-      int cellsBlue = 0;
-      
-      for(map<pair<int,int>,int>::iterator it=neighborsState.begin(); it!=neighborsState.end();++it)
+	cout<<"inside plugin method nextgeneration() OF PLUGINWAR that outputs the next gen of the grid"<<endl;
+	vector<int> temp;
+	bool fought = false;
+	bool redMove = false;
+	bool blueMove = false;
+	int cpt = 0;
+
+	//init the temp vector with 0 state
+	for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++)
 	{
-	  
-	  if(it->second == 2)
-	    {
-	      cellsBlue++;
-	    }
-	  if(it->second == 3)
-	    {
-	      cellsRed++;
-	    }
-	  
-	  if (cpt == 3)
-	    {
-	      if (it->second == 2)
-		{
-		  blueMove = true;
-		}
-	    }
-	  
-	  if (cpt == 4)
-	    {
-	      if (it->second == 3)
-		{
-		  redMove = true;
-		}
-	    }
-	  
-	  cpt++;
+		temp.push_back(0);
 	}
-      
-      int cellState = (gr.getTab())[ind]->getPion();
-      if(cellState == 2)
+
+	for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++)
 	{
-	  if (cellsRed == 0)
-	    {
-	      fought = false;
-	    }
-	  else
-	    {
-	      fought = true;
-	      
-	      if(cellsRed >= cellsBlue+1)
+
+		map<pair<int,int>,int> neighborsState = gr.getCellNeighborsState((gr.getTab())[ind]);
+		int cellsRed = 0;
+		int cellsBlue = 0;
+
+		for(map<pair<int,int>,int>::iterator it=neighborsState.begin(); it!=neighborsState.end();++it)
 		{
-		  temp[ind] = 0;
+
+			if(it->second == 2)
+			{
+				cellsBlue++;
+			}
+			if(it->second == 3)
+			{
+				cellsRed++;
+			}
+
+			if (cpt == 3)
+			{
+				if (it->second == 2)
+				{
+					blueMove = true;
+				}
+			}
+
+			if (cpt == 4)
+			{
+				if (it->second == 3)
+				{
+					redMove = true;
+				}
+			}
+
+			cpt++;
 		}
-	      if(cellsRed < cellsBlue+1)
+
+		int cellState = (gr.getTab())[ind]->getPion();
+		if(cellState == 2)
 		{
-		  temp[ind] = 2;
+			if (cellsRed == 0)
+			{
+				fought = false;
+			}
+			else
+			{
+				fought = true;
+
+				if(cellsRed >= cellsBlue+1)
+				{
+					temp[ind] = 0;
+				}
+
+				if(cellsRed < cellsBlue+1)
+				{
+					temp[ind] = 2;
+				}
+			}
 		}
-	    }
-	  
+		else if (cellState == 3)
+		{
+			if (cellsBlue == 0)
+			{
+				fought = false;
+			}
+			else
+			{
+				fought = 1;
+
+				if(cellsBlue >= cellsRed+1)
+				{
+				temp[ind] = 0;
+				}
+				if(cellsBlue < cellsRed+1)
+				{
+				temp[ind] = 3;
+				}
+			}
+		}
+		else if (cellState == 0)
+		{
+			if (blueMove && redMove)
+			{
+				temp[ind] = 0;
+			}
+			if (!fought && blueMove)
+			{
+				temp[ind] = 2;
+			}
+			if (!fought && redMove)
+			{
+				temp[ind] = 3;
+			}
+		}
+
 	}
-      else if (cellState == 3)
+
+	for(int ind=0; ind< gr.sizeRows() * gr.sizeColumns();ind++)
 	{
-	  if (cellsBlue == 0)
-	    {
-	      fought = false;
-	    }
-	  else
-	    {
-	      fought = 1;
-	      
-	      if(cellsBlue >= cellsRed+1)
-		{
-		  temp[ind] = 0;
-		}
-	      if(cellsBlue < cellsRed+1)
-		{
-		  temp[ind] = 3;
-		}
-	    }
-	  
+		(gr.getTab())[ind]->setPion(temp[ind]);
 	}
-      else if (cellState == 0)
-	{
-	  if (blueMove && redMove)
-	    {
-	      temp[ind] = 0;
-	    }
-	  if (!fought && blueMove)
-	    {
-	      temp[ind] = 2;
-	    }
-	  
-	  if (!fought && redMove)
-	    {
-	      temp[ind] = 3;
-	    }
-	  
-	  
-	}
-      
-    }
-  
-  for(int ind=0; ind< gr.sizeRows() * gr.sizeColumns();ind++)
-    {
-      (gr.getTab())[ind]->setPion(temp[ind]);
-    }
 }
 
 
