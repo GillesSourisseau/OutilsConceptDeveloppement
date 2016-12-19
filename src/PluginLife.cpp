407116747d2d51102a08b940ea821e14cdd1deb9
AndreiTodor <bvandrei94@gmail.com>
2016-12-19 20:50:55 +0100

    implementation de logique du PluginLife et ajout lines de compilation en Makefile; plugin non encore testé

--- src/PluginLife.cpp
+++ src/PluginLife.cpp
@@ -3,6 +3,8 @@
 #include "SpriteLife.hpp"
 #include <iostream>
 #include <map>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -20,31 +22,53 @@ PluginLife::~PluginLife(){
 
 void PluginLife::nextGeneration(Grid& gr){
   cout<<"inside plugin method nextgeneration() that outputs the next gen of the grid"<<endl;
-  vector<int> temp;
+   vector<int> temp;
+   srand(time(0));
   //init the temp vector with 0 state
   for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++){
     temp.push_back(0);
   }
-  
   for(int ind=0;ind < gr.sizeRows() * gr.sizeColumns();ind++){
-    map<pair<int,int>,int> neighborsState = gr.getCellNeighborsState((gr.getTab())[ind]);
-    int cellsAlive = 0;
-    for(map<pair<int,int>,int>::iterator it=neighborsState.begin(); it!=neighborsState.end();++it){
-      if(it->second == 1){
-	cellsAlive++;
-      }
-    }
+    int sg = 0;
+    int dg = 0; 
+    int io = 0;
     int cellState = (gr.getTab())[ind]->getPion();
-    if(cellState == 0){
-      if(cellsAlive > 2){
-	temp[ind] = 1;
+    vector< pair<int,int> > freeNeighbors;
+    map<pair<int,int>,int> neighborsState = gr.getCellNeighborsState((gr.getTab())[ind]);
+    for(map<pair<int,int>,int>::iterator it=neighborsState.begin(); it!=neighborsState.end();++it){
+      if(it->second == 0){
+	freeNeighbors.push_back(it->first);
       }
-    }else if(cellsAlive == 2 || cellsAlive == 3){
-      temp[ind] = 1;;
-    }else if(cellsAlive < 2 || cellsAlive > 3){
+      if(io == 1 || io == 3 || io == 4 || io == 6){
+	if((it->second == 1 && cellState == 1) || (it->second == 2 && cellState == 2) ) sg++;
+	if((it->second == 1 && cellState == 2) || (it->second == 2 && cellState == 2) ) dg++;
+      }
+      io++;
+    }
+    if( sg==1 && dg==0){
       temp[ind] = 0;
+      continue;
+    }else if( sg==0 && dg==1 ){
+      bool found = false;
+      while(!found){
+	int qrand = rand() % freeNeighbors.size();
+	if(temp[freeNeighbors[qrand].second + gr.sizeColumns() * freeNeighbors[qrand].first] == 0){
+	  temp[freeNeighbors[qrand].second + gr.sizeColumns() * freeNeighbors[qrand].first] = (rand() % 2) + 1;
+	  found = true;
+	}
+      }
+    }else if(sg==0 && dg==0 && cellState!=0){
+      bool found = false;
+      while(!found){
+	int qrand = rand() % freeNeighbors.size();
+	if(temp[freeNeighbors[qrand].second + gr.sizeColumns() * freeNeighbors[qrand].first] == 0){
+	  temp[freeNeighbors[qrand].second + gr.sizeColumns() * freeNeighbors[qrand].first] = cellState;
+	  found = true;
+	}
+      }
     }
   }
+  
   for(int ind=0; ind< gr.sizeRows() * gr.sizeColumns();ind++){
     (gr.getTab())[ind]->setPion(temp[ind]);
   }
@@ -66,7 +90,8 @@ void PluginLife::applyProba(Grid& oldGrid, Grid& newGrid){
 
 Sprite* PluginLife::getControllerSprite(){
   map<int,string> m;
-  m.insert(std::make_pair(0,"white"));
-  m.insert(std::make_pair(1,"black"));
+  m.insert(std::make_pair(0,"black"));
+  m.insert(std::make_pair(1,"red"));
+  m.insert(std::make_pair(2,"green"));
   return new SpriteLife(m);
 }
