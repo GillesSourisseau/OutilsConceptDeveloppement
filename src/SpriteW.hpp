1798c703c92d12e1722a369c0d39cfccd54f9709
Killian GOMES <E126956W@S040V7pc09.ensinfo.sciences.univ-nantes.prive>
2016-12-19 15:05:00 +0100

    Plugin War

---
+++ src/SpriteW.hpp
@@ -0,0 +1,14 @@
+#ifndef __SPRITEW__
+#define __SPRITEW__
+
+#include <string>
+#include "Sprite.hpp"
+
+class SpriteW : public Sprite{
+public:
+  SpriteW();
+  ~SpriteW();
+};
+
+
+#endif //__SPRITEW__
